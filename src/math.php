2885b183b9d7c122b29d1c34ec89386ff4d99b80
barust-a <barust_a@etna-alternance.net>
2018-07-03 10:07:04 +0200

    pubkey

---
+++ src/math.php
@@ -0,0 +1,44 @@
+<?php
+function modulo($int, $n)
+{
+    $mod = $int;
+
+    if ($mod >= 0)
+    {
+        while($mod >= $n)
+            $mod -= $n;
+    }
+    else
+    {
+        while($mod  < -$n)
+            $mod += $n;
+        $mod += $n;
+    }
+    return ($mod);
+}
+
+function inv_modulo($a, $n)
+{
+    for ($i = 2; $i <= $n; $i++)
+    {
+        if (modulo(($i * $a), $n) == 1)
+            return ($i);
+    }
+}
+
+function pgcd($a, $b)
+{
+    while($a > 1)
+    {
+        $c = modulo($a, $b);
+
+        if($c == 0)
+        break;
+
+        $a = $b;
+        $b = $c;
+    }
+    return $b;
+}
+
+?>
---
+++ src/pubkey.php
@@ -0,0 +1,45 @@
+<?php
+include 'math.php';
+
+$array = [1, 2, 5, 10, 20, 50, 100, 200];
+
+function getM($array)
+{
+    $minM = array_sum($array);
+    $M = readline("Entrez M : ");
+    if($M <= $minM)
+    {
+        echo "veuillez entrer un entier plus grand que la somme des valeurs de votre suite \n";
+        getM($array);
+    }
+    return $M;
+}
+
+function getE($M)
+{
+    $E = readline("Entrez E : ");
+    if($E >= $M)
+    {
+        echo "E doit être plus petit que " . $M . "\n";
+        getE($M);
+    }
+    elseif(pgcd($M, $E) != 1)
+    {
+        echo "le PGCD de M et E doit être égal à 1 \n";
+        getE($M);
+    }
+    else
+        return $E;
+}
+
+function finalePubkey($array, $E, $M)
+{
+    foreach ($array as &$value) {
+        $value = modulo(($value * $E), $M); //coder son modulo c'est propre de ouf dans le code ouai ouai
+    }
+    unset($value);
+    sort($array, SORT_NUMERIC);
+    return $array;
+}
+
+?>
